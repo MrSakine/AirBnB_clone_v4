5d4eeed911babf4340fda56849c57acd43126677
MrSakine <msakine20@gmail.com>
2024-03-01 17:50:28 +0000

    [task-7] initial code

---
+++ web_dynamic/static/scripts/101-hbnb.js
@@ -0,0 +1,107 @@
+const $ = window.$;
+$(document).ready(function () {
+  const HOST = '0.0.0.0';
+
+  $.get(`http://${HOST}:5001/api/v1/status/`, data => {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+
+  function updateLocations (states, cities) {
+    const locations = Object.assign({}, states, cities);
+    if (Object.values(locations).length === 0) {
+      $('.locations h4').html('&nbsp;');
+    } else {
+      $('.locations h4').text(Object.values(locations).join(', '));
+    }
+  }
+
+  const states = {};
+  $('.locations ul h2 input[type="checkbox"]').click(function () {
+    if ($(this).is(':checked')) {
+      states[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete states[$(this).attr('data-id')];
+    }
+    updateLocations(states, cities);
+  });
+
+  const cities = {};
+  $('.locations ul ul li input[type="checkbox"]').click(function () {
+    if ($(this).is(':checked')) {
+      cities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete cities[$(this).attr('data-id')];
+    }
+    updateLocations(states, cities);
+  });
+
+  const amenities = {};
+  $('.amenities input[type="checkbox"]').click(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    $('.amenities h4').text(Object.values(amenities).join(', '));
+  });
+
+  function search (filters = {}) {
+    $.ajax({
+      type: 'POST',
+      url: `http://${HOST}:5001/api/v1/places_search`,
+      data: JSON.stringify(filters),
+      dataType: 'json',
+      contentType: 'application/json',
+      success: function (data) {
+        $('SECTION.places').empty();
+        $('SECTION.places').append(data.map(place => {
+          return `<article>
+                    <div class="title_box">
+                      <h2>${place.name}</h2>
+                      <div class="price_by_night">${place.price_by_night}</div>
+                    </div>
+                    <div class="information">
+                      <div class="max_guest">${place.max_guest} Guests</div>
+                      <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+                      <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+                    </div>
+                    <div class="description">
+                      ${place.description}
+                    </div>
+                    <div class=reviews>
+                      <div class="review_title">
+                        <h2>X Reviews</h2>
+                        <span>hide</span>
+                      </div>
+                      <ul>
+                          <li>
+                              <h3>From Bob Dylan the 27th January 2017</h3>
+                              <p>Runshi is an epic host. Nothing more I can say. 5 star!</p>
+                          </li>
+                          <li>
+                              <h3>From Connor the 4th January 2017</h3>
+                              <p>Highly recommended!</p>
+                          </li>
+                      </ul>
+                    </div>
+                  </article>`;
+        }));
+      }
+    });
+  }
+
+  $('#search').click(function () {
+    const filters = {
+      states: Object.keys(states),
+      cities: Object.keys(cities),
+      amenities: Object.keys(amenities)
+    };
+
+    search(filters);
+  });
+  search();
+});
